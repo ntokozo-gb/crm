9d13b1a7b2e64b175bcb66970d208427dd8eca09
Ntokozo Gabela <ntokozodev@gmail.com>
2018-02-02 01:29:10 +0200

    refactor: update models and migrations

--- crm_app/models.py
+++ crm_app/models.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=15)
+
+
 class Client(models.Model):
-    client_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     contact_person = models.CharField(max_length=100)
     contact_number = models.CharField(max_length=100)
-
-
-class Project(models.Model):
-    clients = models.ManyToManyField(Client)
-    proj_name = models.CharField(max_length=100)
-    status = models.CharField(max_length=15)
+    projects = models.ManyToManyField(Project)
